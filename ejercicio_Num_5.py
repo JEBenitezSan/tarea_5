7ce26709d1c73d893e0b9effa6e4b11320876709
JEBenitezSan <joseestebansanchezbenitez9514@gmail.com>
2021-11-15 19:23:37 -0600

    se quito el comentario

--- ejercicio_Num_5.py
+++ ejercicio_Num_5.py
@@ -1,10 +1,5 @@
 def ejercicio_num_5 ():
-   """Desarrolle un programa que, dados diez numeros cualquiera, los vuelva enteros
-   y determine cual de los ultimos nueve numeros es mas cercano al primero.
-   (Ejemplo, si el usuario introduce los numeros 2, 6, 4, 8, 12.5, 9.1, 1, 3, 1 y 10, la
-   funcion respondera que el numero mas cercano al 2 es el 1 -> No se debe de
-   permitir el ingreso de cero y si lo hacemos el programa no tiene porque reiniciar
-   la entrada de todos los numeros."""
+
    def vali_number(valor):
       try: 
          if(int(valor) and int(valor) >= 1):
